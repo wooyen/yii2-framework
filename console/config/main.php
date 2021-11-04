542240003a10263ef1de4b14a631f16f5d246bf1
WU Yan <wuyan@frankyxs.com>
2021-11-04 19:35:10 +0800

    consle config

--- console/config/main.php
+++ console/config/main.php
@@ -1,11 +1,10 @@
 <?php
 $params = array_merge(
 	require __DIR__ . '/../../common/config/params.php',
-	require __DIR__ . '/../../common/config/params-local.php',
+	require __DIR__ . "/../../common/config/params-{$yii_env['ENV']}.php",
 	require __DIR__ . '/params.php',
-	require __DIR__ . '/params-local.php'
+	require __DIR__ . "/params-{$yii_env['ENV']}.php"
 );
-
 return [
 	'id' => 'app-console',
 	'basePath' => dirname(__DIR__),
