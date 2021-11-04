69d6c58823742410f8b26d5647af1aa77ef6925c
WU Yan <wuyan@frankyxs.com>
2021-11-04 19:05:36 +0800

    backend config

--- backend/config/main.php
+++ backend/config/main.php
@@ -1,10 +1,14 @@
 <?php
 $params = array_merge(
 	require __DIR__ . '/../../common/config/params.php',
-	require __DIR__ . '/../../common/config/params-local.php',
+	require __DIR__ . "/../../common/config/params-{$yii_env['ENV']}.php",
 	require __DIR__ . '/params.php',
-	require __DIR__ . '/params-local.php'
+	require __DIR__ . "/params-{$yii_env['ENV']}.php"
 );
+if (($cvk = file_get_contents(__DIR__ . '/../runtime/cookieValidationKey')) === false) {
+	$cvk = strtr(base64_encode(openssl_random_pseudo_bytes(24)), '+/=', '_-.');
+	file_put_contents(__DIR__ . '/../runtime/cookieValidationKey', $cvk);
+}
 
 return [
 	'id' => 'app-backend',
@@ -15,6 +19,7 @@ return [
 	'components' => [
 		'request' => [
 			'csrfParam' => '_csrf-backend',
+			'cookieValidationKey' => $cvk,
 		],
 		'user' => [
 			'identityClass' => 'common\models\User',
--- backend/web/index.php
+++ backend/web/index.php
@@ -1,6 +1,5 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+require __DIR__ . '/../../common/config/env.php';
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
@@ -9,9 +8,9 @@ require __DIR__ . '/../config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
 	require __DIR__ . '/../../common/config/main.php',
-	require __DIR__ . '/../../common/config/main-local.php',
+	require __DIR__ . "/../../common/config/main-{$yii_env['ENV']}.php",
 	require __DIR__ . '/../config/main.php',
-	require __DIR__ . '/../config/main-local.php'
+	require __DIR__ . "/../config/main-{$yii_env['ENV']}.php"
 );
 
 (new yii\web\Application($config))->run();
