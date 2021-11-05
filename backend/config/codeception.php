c9797a59f69fffad848921d5cfcd472c772094bf
WU Yan <wuyan@frankyxs.com>
2021-11-05 18:16:27 +0800

    backend codeception

---
+++ backend/config/codeception.php
@@ -0,0 +1,11 @@
+<?php
+
+return yii\helpers\ArrayHelper::merge(
+	require dirname(dirname(__DIR__)) . '/common/config/codeception.php',
+	require __DIR__ . '/main.php',
+	require __DIR__ . "/main-{$yii_env['ENV']}.php",
+	require __DIR__ . '/test.php',
+	require __DIR__ . "/test-{$yii_env['ENV']}.php",
+	[
+	]
+);
