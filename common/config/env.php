d7cfff724b2cff9b4f39475f13a6bf0c199714e3
WU Yan <wuyan@frankyxs.com>
2021-11-30 10:14:29 +0800

    env file

--- common/config/env.php
+++ common/config/env.php
@@ -1,19 +1,42 @@
 <?php
-$env_name = 'YII2_INI_FILE';
 $default_ini_file = '.yii2.ini';
 $ini_file = getenv('YII2_INI_FILE');
 if (empty($ini_file)) {
-	if (!empty($home = getenv('HOME')) && is_readable("$home/$default_ini_file")) {
+	if (!empty($home = getenv('HOME')) && is_file("$home/$default_ini_file")) {
 		$ini_file = "$home/$default_ini_file";
-	} else if (is_readable("/etc/$default_ini_file")) {
+	} else if (is_file("/etc/$default_ini_file")) {
 		$ini_file = "/etc/$default_ini_file";
 	}
-} else if (!is_readable($ini_file)) {
+} else if (!is_file($ini_file)) {
 	die("The specified ini file $ini_file does not exist or is not readable.");
 }
-$yii_env = @parse_ini_file($ini_file);
-if ($yii_env === false) {
-	$yii_env = [];
+if (!empty($ini_file)) {
+	$yii_env = parse_ini_file($ini_file);
+	if ($yii_env === false) {
+		die("Can not parse the ini file $ini_file.");
+	}
+	$loaded_ini_files = [$ini_file];
+}
+if (array_key_exists('INI_DIR', $yii_env)) {
+	$ini_dir = $yii_env['INI_DIR'];
+	if (substr($ini_idr, 0, 1) != '/') {
+		$ini_dir = dirname($ini_file) . '/' . $ini_dir;
+	}
+	$dir = dir($ini_dir);
+	if ($dir === false) {
+		die("Can not open directory $ini_dir.");
+	}
+	while (($entry = $dir->read()) !== false) {
+		if (preg_match('/^[^\.].*\.ini$/', $entry) > 0) {
+			$file = "$ini_dir/$entry";
+			$loaded_ini_files[] = $file;
+			$res = parse_ini_file($file);
+			if ($res === false) {
+				die("Can not parse the ini file $file.");
+			}
+			$yii_env = array_merge($yii_env, $res);
+		}
+	}
 }
 if (!array_key_exists('ENV', $yii_env)) {
 	$yii_env['ENV'] = 'dev';
